79aee37d30e2fa827060ee8df3904c8870ab6e64
Filip Brychta <fbrychta@redhat.com>
2018-08-15 16:38:59 +0200

    Add more tests to group

--- kiali_qe/tests/test_services_page.py
+++ kiali_qe/tests/test_services_page.py
@@ -26,13 +26,13 @@ def test_filter_feature_random(kiali_client, openshift_client, browser):
         kiali_client=kiali_client, openshift_client=openshift_client, browser=browser)
     tests.assert_filter_feature_random()
 
-
+@pytest.mark.group4
 def test_all_services(kiali_client, openshift_client, browser):
     tests = ServicesPageTest(
         kiali_client=kiali_client, openshift_client=openshift_client, browser=browser)
     tests.assert_all_items(filters=[])
 
-
+@pytest.mark.group4
 def test_service_details_random(kiali_client, openshift_client, browser):
     tests = ServicesPageTest(
         kiali_client=kiali_client, openshift_client=openshift_client, browser=browser)
